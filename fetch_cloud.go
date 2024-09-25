bb4c7a688819c9dc1dd218906522eabf71e3882e
adofm <karthik.maniam008@gmail.com>
2024-09-25 20:57:25 +0530

    feature : extracted the data using datamodels for cloud

--- fetch_cloud.go
+++ fetch_cloud.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
-func FetchCloudData(lat,lon,apiKey string) (*http.Response, error) {
+func FetchCloudData(lat, lon, apiKey string) (*http.Response, error) {
 	url := fmt.Sprintf("http://my.meteoblue.com/packages/clouds-1h_clouds-day?lat=%s&lon=%s&apikey=%s", lat, lon, apiKey)
 	client := http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Get(url)
@@ -21,17 +22,32 @@ func FetchCloudData(lat,lon,apiKey string) (*http.Response, error) {
 func handlerGetCloud(w http.ResponseWriter, r *http.Request, apiKey string) {
 	lat := r.URL.Query().Get("lat")
 	lon := r.URL.Query().Get("lon")
-	res,err :=FetchCloudData(lat,lon,apiKey)
+	res, err := FetchCloudData(lat, lon, apiKey)
 
-	if err!=nil{
-		http.Error(w,"Failed to fetch data"	,http.StatusInternalServerError)
+	if err != nil {
+		http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
 		return
 	}
 	defer res.Body.Close()
-	err=WriteJSONToFile(res)
-	if err!=nil{
-		http.Error(w,"Failed to write JSON",http.StatusInternalServerError)
+	filePath, err := WriteJSONToFile(res)
+	if err != nil {
+		http.Error(w, "Failed to write JSON", http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "Cloud data fetched and saved successfully")
+	// extracting the cloud data
+
+	var params Cloud
+	dat, err := extractData(filePath, &params)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Println(err)
+		return
+	}
+	// Sending the Response
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(200)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Println("Couldn't write to response body", err)
+	}
 }
